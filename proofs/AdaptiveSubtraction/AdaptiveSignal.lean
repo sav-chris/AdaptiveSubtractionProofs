604da354849c27ac806fd4648185d0960e3847be
sav-chris <christopher.savini@gmail.com>
2025-12-28 21:15:25 +1000

    Progress on translation invariance proof

--- proofs/AdaptiveSubtraction/AdaptiveSignal.lean
+++ proofs/AdaptiveSubtraction/AdaptiveSignal.lean
@@ -86,7 +86,7 @@ noncomputable def S{n:ℕ}
     (x : EuclideanSpace ℝ (Fin n))
     (τ : ℝ)
     (lower upper :  EuclideanSpace ℝ (Fin n))
-    (Ω :  Set (EuclideanSpace ℝ (Fin n)) := (hypercube lower upper))
+    (Ω : Set (EuclideanSpace ℝ (Fin n)) := (hypercube lower upper))
 :
     ℝ
 :=
@@ -96,7 +96,7 @@ noncomputable def S{n:ℕ}
 noncomputable def Ψ
     {n : ℕ }
     (f : EuclideanSpace ℝ (Fin n) → ℝ)
-    (Ω :  Set (EuclideanSpace ℝ (Fin n)) )
+    (Ω : Set (EuclideanSpace ℝ (Fin n)) )
 :
     ℝ
 :=
@@ -138,7 +138,15 @@ lemma Ψ_linear_operator_scale
 {
     unfold Ψ
 
-    simp_all only [smul_eq_mul, integral_const, MeasurableSet.univ, measureReal_restrict_apply, univ_inter, mul_one]
+    simp_all only
+    [
+        smul_eq_mul,
+        integral_const,
+        MeasurableSet.univ,
+        measureReal_restrict_apply,
+        univ_inter,
+        mul_one
+    ]
 
     let vol := volume.real Ω
     change (∫ x in Ω, α * f x) / vol = α * ((∫ x in Ω, f x) / vol)
@@ -150,4 +158,130 @@ lemma Ψ_linear_operator_scale
     field_simp [hΩ_local]
 
     exact integral_const_mul α f
+}
+
+
+noncomputable def S_Ψ{n : ℕ}
+    (I B : EuclideanSpace ℝ (Fin n) → ℝ)
+    (lower upper : EuclideanSpace ℝ (Fin n))
+    (Ω : Set (EuclideanSpace ℝ (Fin n)) := (hypercube lower upper))
+    (x : EuclideanSpace ℝ (Fin n))
+    (τ : ℝ)
+:
+    ℝ
+:=
+    (μ_full I B  lower upper Ω) + (I x) - (Ψ (λ p ↦ (ρ I B p τ) • (B x)) (G x τ )) - (Ψ (λ p ↦ (μ p I B τ)) (G x τ ) )
+
+
+noncomputable def S_Ψ_1{n : ℕ}
+    (I B : EuclideanSpace ℝ (Fin n) → ℝ)
+    (lower upper : EuclideanSpace ℝ (Fin n))
+    (Ω : Set (EuclideanSpace ℝ (Fin n)) := (hypercube lower upper))
+    (x : EuclideanSpace ℝ (Fin n))
+    (τ : ℝ)
+:
+    ℝ
+:=
+    (μ_full I B lower upper Ω) +
+    (I x) -
+    (B x) • (Ψ (λ p ↦ (ρ I B p τ)) (G x τ )) -
+    (Ψ
+      (
+        λ p ↦
+        (Ψ
+          (λ q ↦ (I q))
+          (G p τ)
+        )
+      )
+      (G x τ)
+    ) -
+    (Ψ
+      (λ p ↦ (ρ I B p τ) •
+        (Ψ
+          (λ q ↦ (B q))
+          (G p τ )
+        )
+      )
+      (G x τ )
+    )
+
+
+
+
+
+lemma G_vol_translates
+    {n : ℕ }
+    (τ : ℝ )
+    (hτ: τ > 0 )
+:
+    ∀ (x : EuclideanSpace ℝ (Fin n)), (G_abs x τ) = (G_abs (0 : EuclideanSpace ℝ (Fin n)) τ)
+:= by
+{
+    intro x
+    simp only [gt_iff_lt] at hτ
+    unfold G_abs
+    unfold G
+
+    simp only [sub_zero, setOf_mem_eq]
+
+    change  ∫ x in {p | p - x ∈ G_0 τ}, 1 = ∫ x in G_0 τ, 1
+    simp only [sub_eq_add_neg]
+
+    let vol : Set (EuclideanSpace ℝ (Fin n)) := (G_0 τ)
+
+    change ∫ x in {p | p + -x ∈ vol}, 1 = ∫ x in vol, 1
+
+    let a := -x
+    change ∫ x in {p | p + a ∈ vol}, 1 = ∫ x in vol, 1
+
+    simp only [integral_const, MeasurableSet.univ, measureReal_restrict_apply, univ_inter, smul_eq_mul, mul_one]
+
+    change volume.real ((λ p ↦ p + a) ⁻¹' vol) = volume.real vol
+
+    change (volume ((fun p ↦ p + a) ⁻¹' vol)).toReal = (volume vol).toReal
+
+    simp only [measure_preimage_add_right]
+}
+
+
+lemma grad_Ψ
+    {n : ℕ }
+    (f : EuclideanSpace ℝ (Fin n) → ℝ)
+    (Ω : Set (EuclideanSpace ℝ (Fin n)) )
+    --(x : EuclideanSpace ℝ (Fin n))
+    (τ : ℝ)
+    (hτ : τ > 0)
+:
+    ∇ (λ x ↦ Ψ f (G x τ )) = ∇ (λ x ↦ Ψ (λ p ↦ (f (p + x) )) (G 0 τ ))
+:= by
+{
+    unfold Ψ
+
+    simp_all only
+    [
+        smul_eq_mul,
+        integral_const,
+        MeasurableSet.univ,
+        measureReal_restrict_apply,
+        univ_inter,
+        mul_one
+    ]
+
+    let vol(q : EuclideanSpace ℝ (Fin n)) := volume.real (G q τ)
+
+    change (∇ fun x ↦ (∫ x in G x τ, f x) / (vol x) ) = ∇ fun x ↦ (∫ p in G 0 τ, f (p + x)) / (vol 0)
+
+    --change (∫ x in Ω, α * f x) / vol = α * ((∫ x in Ω, f x) / vol)
+/-
+    have hΩ_local : vol ≠ 0 := by
+    {
+        simp_all only [ne_eq, (vol q)]
+        trace_state
+    }
+    field_simp [hΩ_local] -/
+    --simp_all only [gt_iff_lt]
+
+    trace_state
+
+
 }
